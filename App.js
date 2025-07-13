3c98b3dd107e5c58af28ed8e9a14d814bdb5a585
unknown <nikhil.verma12111@gmail.com>
2025-07-13 17:46:09 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import FormikAndYup from './src/screens/formikAndYup'
+
+const App = () => {
+  return (
+    <View>
+      <FormikAndYup/>
+    </View>
+  )
+}
+
+export default App
---
+++ src/screens/formikAndYup/index.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FormikAndYup = () => {
+  return (
+    <View style={styles.mainView}>
+      
+    </View>
+  )
+}
+
+export default FormikAndYup
+
+const styles = StyleSheet.create({
+    mainView:{
+        flex:1,
+
+    }
+})
